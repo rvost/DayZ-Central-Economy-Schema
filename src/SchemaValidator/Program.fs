b07b47095a8b3cf1c33d6901f03fea27506ceeb1
rvost <romanvost@gmail.com>
2023-11-11 03:33:23 +0300

    feat: Scaffold validator project

---
+++ src/SchemaValidator/Program.fs
@@ -0,0 +1,64 @@
+﻿open System.IO
+open System.Xml
+open System.Xml.Schema
+open System.Xml.Linq
+open SchemaValidator.Schema
+
+let load (path: string) =
+    try
+        let doc = XDocument.Load(path, LoadOptions.SetBaseUri ||| LoadOptions.SetLineInfo)
+        Ok(doc)
+    with ex ->
+        Error([| ex.Message |])
+
+let validate (schema: XmlSchemaSet) (doc: XDocument) =
+    let errors = ResizeArray<string>()
+
+    let handler (sender:obj) (args: ValidationEventArgs) =
+        let lineInfo = sender :?> IXmlLineInfo
+        errors.Add($"{args.Severity}: {args.Exception.Message} [Ln {lineInfo.LineNumber} Col {lineInfo.LinePosition}]")
+
+    doc.Validate(schema, handler)
+
+    if errors.Count = 0 then
+        Ok(doc)
+    else
+        Error(errors.ToArray())
+
+let validateXsd (xsdLoader) (doc: XDocument) =
+    match resolveXsd (doc.BaseUri) with
+    | Some schema -> validate (xsdLoader (schema)) doc
+    | None -> Error([| $"Schema not found for {doc.BaseUri}" |])
+
+let processFile (xsdLoader) (filePath: string) =
+    let result = filePath |> load |> Result.bind (validateXsd xsdLoader)
+
+    match result with
+    | Ok _ ->
+        printfn "%s successfully validated" filePath
+        0
+    | Error errors ->
+        printfn "Errors in %s" filePath
+        Seq.iter (printfn "\t%s") errors
+        errors.Length
+
+let localXsdLoader (xsdRoot: string) (path: string) =
+    let schemaSet = XmlSchemaSet()
+    let fullPath = Path.Combine(xsdRoot, path)
+    schemaSet.Add(null, XmlReader.Create(fullPath)) |> ignore
+    schemaSet
+
+[<EntryPoint>]
+let main argv =
+
+    let directoryPath = Array.head argv
+    let schemaRoot = argv |> Array.tryItem 1 |> Option.defaultValue "./schema"
+
+    let xmlFiles =
+        Directory.EnumerateFiles(directoryPath, "*.xml", SearchOption.AllDirectories)
+
+    let xsdLoader = localXsdLoader schemaRoot
+
+    let errors = xmlFiles |> Seq.map (processFile xsdLoader) |> Seq.sum
+
+    errors
---
+++ src/SchemaValidator/Schema.fs
@@ -0,0 +1,34 @@
+module SchemaValidator.Schema
+open GlobExpressions
+
+let private (|Pattern|_|) (pattern: string) input =
+    if Glob.IsMatch(input, pattern, GlobOptions.CaseInsensitive) then
+        Some()
+    else
+        None
+
+let resolveXsd (path: string) =
+    match path with
+    | Pattern "**/cfgeconomycore.xml" -> Some("cfgeconomycore.xsd")
+    | Pattern "**/cfgenvironment.xml" -> Some("cfgenvironment.xsd")
+    | Pattern "**/cfgeventgroups.xml" -> Some("cfgeventgroups.xsd")
+    | Pattern "**/cfgeventspawns.xml" -> Some("cfgeventspawns.xsd")
+    | Pattern "**/cfgignorelist.xml" -> Some("cfgignorelist.xsd")
+    | Pattern "**/cfglimitsdefinition.xml" -> Some("cfglimitsdefinition.xsd")
+    | Pattern "**/cfglimitsdefinitionuser.xml" -> Some("cfglimitsdefinitionuser.xsd")
+    | Pattern "**/cfgplayerspawnpoints.xml" -> Some("cfgplayerspawnpoints.xsd")
+    | Pattern "**/cfgrandompresets.xml" -> Some("cfgrandompresets.xsd")
+    | Pattern "**/cfgspawnabletypes.xml" -> Some("cfgspawnabletypes.xsd")
+    | Pattern "**/cfgweather.xml" -> Some("cfgspawnabletypes.xsd")
+    | Pattern "**/mapclusterproto.xml" -> Some("mapclusterproto.xsd")
+    | Pattern "**/mapgroupcluster*.xml" -> Some("mapgroupcluster.xsd")
+    | Pattern "**/mapgroupdirt.xml" -> Some("mapgroupdirt.xsd")
+    | Pattern "**/mapgrouppos.xml" -> Some("mapgrouppos.xsd")
+    | Pattern "**/mapgroupproto.xml" -> Some("mapgroupproto.xsd")
+    | Pattern "**/db/economy.xml" -> Some("db/economy.xsd")
+    | Pattern "**/db/events.xml" -> Some("db/events.xsd")
+    | Pattern "**/db/globals.xml" -> Some("db/globals.xsd")
+    | Pattern "**/db/messages.xml" -> Some("db/messages.xsd")
+    | Pattern "**/types*.xml" -> Some("db/types.xsd")
+    | Pattern "**/env/*.xml" -> Some("env/territories.xsd")
+    | _ -> None
